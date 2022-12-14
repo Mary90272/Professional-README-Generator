53fe456e1767983b78262d2efe2905bf82820132
Mary <114884929+Mary90272@users.noreply.github.com>
2022-12-13 22:44:03 -0800

    Update generateMarkdown.js
    added double brackets

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -11,16 +11,16 @@ function renderLicenseBadge(license) {
   if (license !== 'No license') {
     switch (license) {
       case 'Apache 2.0':
-        licenseBadge = `![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)`;
+        licenseBadge = `[![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)]`;
         break;
       case 'BSD 3':
-        licenseBadge = `![License] (https://img.shields.io/badge/License-BSD_3--Clause-blue.svg)`;
+        licenseBadge = `[![License] (https://img.shields.io/badge/License-BSD_3--Clause-blue.svg)]`;
         break;
       case 'GNU GPLv3':
-        licenseBadge = `![License] (https://img.shields.io/badge/License-GPLv3-blue.svg)`;
+        licenseBadge = `[![License] (https://img.shields.io/badge/License-GPLv3-blue.svg)]`;
         break;
       case 'MIT':
-        licenseBadge = `![License] https://img.shields.io/badge/License-MIT-yellow.svg)`;
+        licenseBadge = `[![License] https://img.shields.io/badge/License-MIT-yellow.svg)]`;
         break;
       default:
         break;
